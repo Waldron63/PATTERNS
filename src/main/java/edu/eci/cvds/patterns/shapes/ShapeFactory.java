70f1b9031ab9f83fccd40fc8ac39ec1e4f164641
Fatal Tester <juanm46891@gmail.com>
2025-02-01 18:46:55 -0500

    shapefactory

--- src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
+++ src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -1,1 +1,24 @@
- 
+package edu.eci.cvds.patterns.shapes;
+import edu.eci.cvds.patterns.shapes.concrete.*;
+
+public class ShapeFactory {
+    public static Shape create(Shape type){
+        switch (type){
+            case Triangle:
+                return new Triangle();
+                break;
+            case Quadrilateral:
+                return new Quadrilateral();
+                break;
+            case Pentagon:
+                return new Pentagon();
+                break;
+            case Hexagon:
+                return new Hexagon();
+                break;
+            default:
+                return null;
+                break;
+        }
+    }
+}
