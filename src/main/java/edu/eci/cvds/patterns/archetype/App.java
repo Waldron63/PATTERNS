716fc555e40ec71f04d1e0b8cfa699c2068b8a20
Santiago Gualdron <santiago.gualdron-r@mail.escuelaing.edu.co>
2025-02-01 16:19:16 -0500

    saludo en App java

--- src/main/java/edu/eci/cvds/patterns/archetype/App.java
+++ src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -4,10 +4,13 @@ package edu.eci.cvds.patterns.archetype;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        if (args.length != 0){
+            System.out.println("Hello " + String.join(" ",args));
+        }else{
+            System.out.println( "Hello World!");
+        }
     }
 }
